fd10ef8ede2620df6cfdf35ff0c8bd77044e6f80
Fabian Schmengler <fs@integer-net.de>
2017-11-14 11:28:33 +0100

    Improve PHPUnit 4 compatibility layer
    Using a class alias for better IDE support with PHPUnit 6

--- src/phpunit4.php
+++ src/phpunit4.php
@@ -1,17 +1,15 @@
 <?php
 
-namespace PHPUnit\Framework {
-
-    if (!class_exists(TestCase::class)) {
-        /*
-         * Forward compatibility for PHPUnit 4.x (Magento 2.1)
-         */
-        abstract class TestCase extends \PHPUnit_Framework_TestCase
+/*
+ * Forward compatibility for PHPUnit 4.x (Magento 2.1)
+ */
+if (!class_exists(\PHPUnit\Framework\TestCase::class)) {
+    class_alias(\PHPUnit\Framework\TestCase::class, PHPUnit4_TestCase::class);
+    abstract class PHPUnit4_TestCase extends \PHPUnit_Framework_TestCase
+    {
+        public function expectException($exceptionName)
         {
-            public function expectException($exceptionName)
-            {
-                $this->setExpectedException($exceptionName);
-            }
+            $this->setExpectedException($exceptionName);
         }
     }
 }
